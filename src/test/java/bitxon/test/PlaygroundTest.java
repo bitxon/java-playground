02f0631bc999bb670741ffa831b4a88d41a7a47b
Nikita Karpuk <bitxon.dev@gmail.com>
2024-01-29 20:42:58 +0100

    Base project

---
+++ src/test/java/bitxon/test/PlaygroundTest.java
@@ -0,0 +1,13 @@
+package bitxon.test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PlaygroundTest {
+    @Test
+    void play() {
+        // Play with me
+        assertThat(0).isZero();
+    }
+}
